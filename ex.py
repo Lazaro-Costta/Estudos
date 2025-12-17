2be4fed1e40cf638adca8b5db1311a0e4f73f756
Lazaro-Costta <lasarocosta11@gmail.com>
2025-12-17 11:50:07 -0300

    começando

--- ex.py
+++
@@ -1,5 +0,0 @@
-list = []
-list.append(str(input('digite seu nome:')))
-print(list[0])
-lista_filmes = {'comedia': 'todo mundo impanico', 'ação': 'vingadores', 'terror': 'A freira'}
-print(lista_filmes['terror'])
